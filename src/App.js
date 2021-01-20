378ba2f9a19ab6604cf10b3a197833923a51ddae
Junior Silva <jroliveira.sa@outlook.com>
2021-01-20 13:00:10 -0300

    Termindando lógica

--- src/App.js
+++ src/App.js
@@ -7,7 +7,20 @@ import Conversor from './components/Conversor'
 function App() {
   return (
     <div className="App">
-      <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+      <h1>Conversor de Moedas</h1>
+      <div className="grades">
+        <Conversor moedaA="USD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="USD"></Conversor>
+      </div>
+      <div className="grades">
+        <Conversor moedaA="CAD" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="CAD"></Conversor>
+      </div>
+      <div className="grades">
+        <Conversor moedaA="EUR" moedaB="BRL"></Conversor>
+        <Conversor moedaA="BRL" moedaB="EUR"></Conversor>
+      </div>
+
     </div>
   );
 }
--- src/components/Conversor.js
+++ src/components/Conversor.js
@@ -1,19 +1,32 @@
 import React, { Component } from 'react'
-
+import './conversor.css'
 
 export default class Conversor extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
- 
+
         this.state = {
-            moedaA_valor:"",
+            moedaA_valor: "",
             moedaB_valor: 0,
         }
+
+        this.converter = this.converter.bind(this)
     }
 
-    converter(){
-        console.log("convertido")
+    converter() {
+        let de_para = `${this.props.moedaA}_${this.props.moedaB}`;
+
+        let url = `https://free.currconv.com/api/v7/convert?q=${de_para}&compact=ultra&apiKey=cc5d19b6304453cb10e2`
+
+        fetch(url).then(res => {
+            return res.json()
+        })
+            .then(json => {
+                let cotacao = json[de_para];
+                let moedaB_valor = (this.state.moedaA_valor * cotacao).toFixed(2)
+                this.setState({ moedaB_valor })
+            })
 
     }
 
@@ -21,9 +34,9 @@ export default class Conversor extends Component {
         return (
             <div className="conversor">
                 <h2>{this.props.moedaA} para {this.props.moedaB}</h2>
-                <input type="text" onChange={(e)=>{this.setState({moedaA_valor: e.target.value})}}></input>
-                <input type="button" value="Converter" onClick={this.converter}></input>
-                <h2>Valor Convertido</h2>
+                <input type="text" placeholder="Digite o valor" onChange={(e) => { this.setState({ moedaA_valor: e.target.value }) }}></input>
+                <button value="Converter" onClick={this.converter}>Converter</button>
+                <h2>{this.state.moedaB_valor}</h2>
             </div>
         )
     }
